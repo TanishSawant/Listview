3328b99e2387c435155d400afb3a71c461f0e2dc
TANISH SAWANT <53378970+TanishSawant@users.noreply.github.com>
2020-06-17 11:54:45 +0530

    new

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
+import 'package:solid_bottom_sheet/solid_bottom_sheet.dart';
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  SolidController _controller = SolidController();
   final List<String> name = <String>['Aby'];
   final List<String> assignmentNumber = <String>['1'];
   final List<String> description = <String>['The overwhelming assignment'];
@@ -34,7 +35,9 @@ class _State extends State<MyApp> {
 
   bool _isCr = true;
 
-  showAlertDialog(BuildContext context) {
+  
+
+  showAlertDialog1(BuildContext context) {
   // set up the buttons
   Widget okButton = FlatButton(
     child: Text("OK"),
@@ -64,6 +67,8 @@ class _State extends State<MyApp> {
     ],
   );
 
+
+
   // show the dialog
   showDialog(
     context: context,
@@ -73,34 +78,61 @@ class _State extends State<MyApp> {
   );
 }
 
-  String _value = '';
+  showAlertDialog2(BuildContext context, int index) {
+  // set up the buttons
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed:  () {Navigator.pop(context);},
+  );
+  Widget cancelButton = FlatButton(
+    child: Text("Cancel"),
+    onPressed:  () {},
+  );
+  Widget launchButton = FlatButton(
+    child: Text("Launch"),
+    onPressed:  () {},
+  );
 
-  Future _selectDate() async {
-    DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: new DateTime.now(),
-        firstDate: new DateTime(2016),
-        lastDate: new DateTime(2025)
-    );
-    if(picked != null) setState(() => _value = _dateFormat.format(picked));
-  }
+  Widget sorryButton = FlatButton(
+    child: Text("I am SORRY"),
+    onPressed:  () {Navigator.pop(context);},
+  );
 
-  DateFormat _dateFormat = DateFormat("yyyy-MM-dd");
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      appBar: AppBar(
-        title: Text('Assignment'),
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+      title: Text("${name[index].toUpperCase()}",
+      textAlign: TextAlign.center,
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
+      content: Text("${description[index]} \n\n Submission date : $_value",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontFamily: "Poppins" , color: Colors.red, fontSize: 18.0),
+      ),
+      actions: [FlatButton(onPressed: () => Navigator.pop(context), child: Text("ok"))],
+  );
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+
+
+
+  void _settingModalBottomSheet(context){
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext bc){
+          return Container(
+            child:Padding(
             padding: EdgeInsets.all(30),
             child: Visibility(
                 visible: _isCr,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                     Container(
                     margin: EdgeInsets.only(top: 10.0),
@@ -146,49 +178,178 @@ class _State extends State<MyApp> {
                       onPressed: () => _selectDate(),
                     )
                   ),
+                  Visibility(
+                    visible: _isCr,
+                    child:FloatingActionButton(
+                    child: Text('Add'),
+                    onPressed: () {
+                      if(nameController.text != ''){addItemToList(); Navigator.pop(context);}
+                      else {showAlertDialog1(context);}
+                      },
+                    ), 
+                  ),
                 ],
               ),
             ),
           ),
-          Visibility(
-            visible: _isCr,
-            child:FloatingActionButton(
-            child: Text('Add'),
-            onPressed: () {
-              if(nameController.text != ''){addItemToList();}
-              else {showAlertDialog(context);}
-            },
-          ), 
-        ),
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: name.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  //height: 50,
-                  margin: EdgeInsets.all(15),
-                  //color: description[index]>=10? Colors.blue[400]:
-                    //description[index]>3? Colors.blue[100]: Colors.grey,
-                  child: SafeArea(
-                    child: Column(
-                      children: <Widget>[
-                        Text('${name[index] } (${assignmentNumber[index]})',
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        Text('${description[index]}',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                          Text('$_value',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ],
+          
+          );
+      }
+    );
+}
+
+  String _value = '';
+
+  Future _selectDate() async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2025)
+    );
+    if(picked != null) setState(() => _value = _dateFormat.format(picked));
+  }
+
+  DateFormat _dateFormat = DateFormat("yyyy-MM-dd");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      appBar: AppBar(
+        title: Text('Assignment'),
+      ),
+      body: Column(
+        children: <Widget>[
+                  Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: name.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        
+                        return /*Container(
+                          //height: 50,
+                          margin: EdgeInsets.all(15),
+                          //color: description[index]>=10? Colors.blue[400]:
+                            //description[index]>3? Colors.blue[100]: Colors.grey,
+                          child: SafeArea(
+                            child: Column(
+                              children: <Widget>[
+                                Text('${name[index] } (${assignmentNumber[index]})',
+                                  style: TextStyle(fontSize: 24),
+                                ),
+                                Text('${description[index]}',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                  Text('$_value',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            )
+                          ),
+                        );*/
+                        ListTile(
+                          leading: Icon(Icons.assignment, color: Colors.deepOrange,),
+                          title: new Text('${name[index] } (${assignmentNumber[index]})',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          subtitle: Text('$_value',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                          onTap: ()=>{showAlertDialog2(context, index)},
+                        );
+                      }
                     )
                   ),
-                );
-              }
-            )
-          )
+                 Visibility(
+                    visible: _isCr,
+                    child:Container(
+                      margin: EdgeInsets.only(left: 330.0, bottom: 20.0),
+                      child: FloatingActionButton(
+                      child: Text('+'),
+                      onPressed: () => {_settingModalBottomSheet(context)},
+                                ),
+                                  ), 
+                              ),
+                            SolidBottomSheet(
+                              controller: _controller,
+                              draggableBody: true,
+                              headerBar: Container(
+                                height: 50.0,
+                                color: Theme.of(context).primaryColor,
+                                child: Center(
+                                  child: Text("Add Assignment"),
+                                ),
+                              ),
+                              body: SingleChildScrollView(
+                                  child: Container(
+                                  child: Padding(
+                          padding: EdgeInsets.all(30),
+                          child: Visibility(
+                              visible: _isCr,
+                              child: Column(
+                              children: <Widget>[
+                                  Container(
+                                  margin: EdgeInsets.only(top: 10.0),
+                                  child: TextField(
+                                      controller: nameController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: 'Subject',
+                                      ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10.0),
+                                  child: TextField(
+                                      controller: assignmentNumberController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: 'AssignmentNumber',
+                                      ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top : 10.0),
+                                  child: TextField(
+                                      controller: descriptionController,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: 'Description of assignment',
+                                      ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Text(_value,
+                                  style: TextStyle(fontSize: 24.0),
+                                  ),
+                                ),
+                                Container(
+                                  child: IconButton(
+                                  icon: new Icon(
+                                  Icons.date_range,
+                                  color: Colors.black,
+                                ),
+                                      onPressed: () => _selectDate(),
+                                  )
+                                ),
+                          Visibility(
+                            visible: _isCr,
+                            child:FloatingActionButton(
+                            child: Text('Add'),
+                            onPressed: () {
+                              if(nameController.text != ''){addItemToList();}
+                              else {showAlertDialog1(context);}
+                            },
+                          ), 
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ]
       )
     );
